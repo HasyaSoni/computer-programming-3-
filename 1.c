49e672615451c413e9bcd78306cd2e66d0cf445b
HasyaSoni <hasyassoni@gmail.com>
2025-09-10 16:45:08 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+
+{
+    float i = 0;
+    int n;
+
+    do{
+        ++i;
+        printf("%.0f\t",i);
+    }
+    while(i<10);
+}
---
+++ 2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+
+{
+    float i = 0;
+    int n;
+
+
+
+    while(i<10)
+        {
+        ++i;
+        printf("%.0f\t",i);
+    };
+}
